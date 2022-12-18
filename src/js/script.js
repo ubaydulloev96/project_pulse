fb2389b1afb0c1a3c1108e6fcd39de3a0f497ed2
Abdulloh <abdulloh.ustb@gmail.com>
2022-12-18 08:24:18 +0500

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1100,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right.png"></button>'
+    });
+});
+      
